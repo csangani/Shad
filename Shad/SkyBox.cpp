bb47da7ac6ca4decf0eeb1deefa84cc1b284601b
unknown <crcsaenz@TRES-CL-20-IMTB.stanford.edu>
2013-06-09 15:55:03 -0700

    Fixed some Skybox shit.

--- Shad/SkyBox.cpp
+++ Shad/SkyBox.cpp
@@ -34,10 +34,10 @@ void SkyBox::draw()
 	glPushMatrix();
 	glLoadIdentity();
 
-	/* get front-bottom-left corner from center */
-	float x = cx_ - size_ / 2;
-	float y = cy_ - size_ / 2;
-	float z = cz_ - size_ / 2;
+	/* get back-bottom-left corner from center */
+	float x = cx_ - size_ / 2.f;
+	float y = cy_ - size_ / 2.f;
+	float z = cz_ - size_ / 2.f;
 
 	glEnable(GL_TEXTURE_2D);
 
@@ -53,19 +53,19 @@ void SkyBox::draw()
 	/* Draw Back side */
 	glBindTexture(GL_TEXTURE_2D, textures_[SkyBoxBack]);
 	glBegin(GL_QUADS);		
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z);
 		glTexCoord2f(1.0f, 0.0f); glVertex3f(x+size_, y,		z);
 		glTexCoord2f(1.0f, 1.0f); glVertex3f(x+size_, y+size_,  z); 
 		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+size_,	z);
-		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z);
 	glEnd();
 
 	/* Draw Left side */
 	glBindTexture(GL_TEXTURE_2D, textures_[SkyBoxLeft]);
 	glBegin(GL_QUADS);		
-		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y+size_,	z);	
-		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+size_,	z+size_); 
 		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z+size_);
-		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,		  y,		z);		
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,		  y,		z);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y+size_,	z);	
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+size_,	z+size_); 		
 	glEnd();
 
 	/* Draw Right side */
@@ -80,19 +80,19 @@ void SkyBox::draw()
 	/* Draw Up side */
 	glBindTexture(GL_TEXTURE_2D, textures_[SkyBoxTop]);
 	glBegin(GL_QUADS);		
-		glTexCoord2f(0.0f, 0.0f); glVertex3f(x+size_, y+size_,  z);
-		glTexCoord2f(1.0f, 0.0f); glVertex3f(x+size_, y+size_,  z+size_); 
-		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y+size_,	z+size_);
-		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+size_,	z);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,       y+size_,  z);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x+size_, y+size_,  z); 
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x+size_, y+size_,	z+size_);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+size_,	z+size_);
 	glEnd();
 
 	/* Draw Down side */
 	glBindTexture(GL_TEXTURE_2D, textures_[SkyBoxBottom]);
 	glBegin(GL_QUADS);		
-		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z);
-		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,		  y,		z+size_);
-		glTexCoord2f(1.0f, 1.0f); glVertex3f(x+size_, y,		z+size_); 
-		glTexCoord2f(0.0f, 1.0f); glVertex3f(x+size_, y,		z);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z+size_);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x+size_, y,		z+size_);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x+size_, y,		z); 
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y,		z);
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, 0);
